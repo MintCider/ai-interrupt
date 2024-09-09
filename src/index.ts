527248afcf7a39dd705a691e24a85cc0a00da0af
Mint Cider <170254502+MintCider@users.noreply.github.com>
2024-09-09 19:10:11 +0800

    release: v0.1.1

--- src/index.ts
+++ src/index.ts
@@ -117,7 +117,7 @@ function main() {
   // 注册扩展
   let ext = seal.ext.find("ai-interrupt");
   if (!ext) {
-    ext = seal.ext.new("ai-interrupt", "MintCider", "0.1.0");
+    ext = seal.ext.new("ai-interrupt", "MintCider", "0.1.1");
 
     registerCommand(ext);
     seal.ext.register(ext);
