4028a9de25c8a5b174b8b915b13013a1938184af
Mint Cider <170254502+MintCider@users.noreply.github.com>
2025-03-20 20:35:04 +0800

    fix: Try to give information when data initialization fails

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -19,21 +19,37 @@ export function initializeStore(ext: seal.ExtInfo) {
     [key: string]: boolean
   } = JSON.parse(storageGet(ext, "switches"));
   memoryStore.set("switches", switches);
+  if (!memoryStore.has("switches") || !Boolean(memoryStore.get("switches"))) {
+    console.log("ai-interrupt: Failed to load data \"switches\", try reload plugins. If this persists, reset plugin storage.");
+  }
   const configs: {
     [key: string]: GroupConfig
   } = JSON.parse(storageGet(ext, "configs"));
   memoryStore.set("configs", configs);
+  if (!memoryStore.has("configs") || !Boolean(memoryStore.get("configs"))) {
+    console.log("ai-interrupt: Failed to load data \"configs\", try reload plugins. If this persists, reset plugin storage.");
+  }
   const histories: {
     [key: string]: ChatHistory
   } = ChatHistory.deserializeFromJson(storageGet(ext, "histories"));
   memoryStore.set("histories", histories);
+  if (!memoryStore.has("histories") || !Boolean(memoryStore.get("histories"))) {
+    console.log("ai-interrupt: Failed to load data \"histories\", try reload plugins. If this persists, reset plugin storage.");
+  }
   const memories: {
     [key: string]: GroupMemory
   } = JSON.parse(storageGet(ext, "memories"));
   memoryStore.set("memories", memories);
+  if (!memoryStore.has("memories") || !Boolean(memoryStore.get("memories"))) {
+    console.log("ai-interrupt: Failed to load data \"memories\", try reload plugins. If this persists, reset plugin storage.");
+  }
 }
 
 export function getData<T = any>(key: string): T | undefined {
+  if (!memoryStore.has(key)) {
+    console.log(`ai-interrupt: Data error, try reload plugins. If this persists, reset plugin storage. Key: ${key}`);
+    return undefined;
+  }
   return memoryStore.get(key) as T;
 }
 
